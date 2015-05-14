0da0fd019dd90f653abb173ea66d90bfaf4a19e7
groot <questions@mikeware.com>
2015-05-13 20:52:43 -0700

    * Completed Compilation Script for Java jar. (Resolves #16)

--- compile.bat
+++ compile.bat
@@ -1,6 +1,25 @@
 @echo off
+echo CLEAN
 del /Q bin\SBAServer.zip
+del /Q bin\SpaceBattle.jar
+rmdir /S /Q doc\client\java_doc
+
+echo BUILDING SERVER
 mkdir bin
 cd SBA_Serv
 python compile.py py2exe
 cd ..
+
+echo BUILDING CLIENT
+cd java_client_src
+
+rmdir /S /Q bin
+javac -cp ..\bin\gson-2.2.jar -d bin src\ihs\apcs\spacebattle\*.java src\ihs\apcs\spacebattle\commands\*.java src\ihs\apcs\spacebattle\games\*.java src\ihs\apcs\spacebattle\networking\*.java src\ihs\apcs\spacebattle\util\*.java
+cd bin
+jar cf ..\..\bin\SpaceBattle.jar ihs
+cd..
+
+echo GENERATE DOCS
+javadoc -public -sourcepath src -classpath "*;bin" -d ..\doc\client\java_doc -windowtitle "IHS AP CS Space Battle" -doctitle "IHS AP CS Space Battle" ihs.apcs.spacebattle ihs.apcs.spacebattle.commands ihs.apcs.spacebattle.games
+
+cd..
