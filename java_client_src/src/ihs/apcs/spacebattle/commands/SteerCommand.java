a1afc529ce8836aaea9a1f3db2c7975e5afe975d
wortzmanb <wortzmanb@issaquah.wednet.edu>
2016-05-25 10:26:10 -0700

    Client side of #121, provide option for ThrustCommand to be blocking.
    Fix issue with BrakeCommand in fix for #123.
    
    First part of fix for #127, add log message if socket is closed when
    sending.

--- java_client_src/src/ihs/apcs/spacebattle/commands/SteerCommand.java
+++ java_client_src/src/ihs/apcs/spacebattle/commands/SteerCommand.java
@@ -19,7 +19,7 @@ public class SteerCommand extends ShipCommand {
 	private boolean BLOCK;
 	
 	/**
-	 * Creates a Blocking command to steer a ship.  
+	 * Creates a blocking command to steer a ship.  
 	 * @param degrees the number of degrees to adjust course
 	 */
 	public SteerCommand(int degrees) {
