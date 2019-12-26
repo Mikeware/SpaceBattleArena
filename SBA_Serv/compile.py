b90126e36bbc396222024aa5b1c9731d54658089
hawkerm <questions@mikeware.com>
2019-12-26 09:24:45 -0800

    Initial cx_freeze packaging switch test
    Missing some dependencies, needs latest master of cx_freeze still for workarounds

--- SBA_Serv/compile.py
+++ SBA_Serv/compile.py
@@ -1,104 +1,68 @@
 # This will create a dist directory containing the executable file, all the data
 # directories. All Libraries will be bundled in executable file.
 #
-# Run the build process by entering 'pygame2exe.py' or
-# 'python pygame2exe.py' in a console prompt.
+# Run the build process by entering
+# 'python compile.py build' in a console prompt.
 #
-# To build exe, python, pygame, and py2exe have to be installed. After
-# building exe none of this libraries are needed.
-#Please Note have a backup file in a different directory as if it crashes you 
-#will loose it all!(I lost 6 months of work because I did not do this)
- 
+# To build the exe, python, pygame, pymunk, and cx_Freeze have to be installed.
+#
+# https://cx-freeze.readthedocs.io/en/latest/distutils.html
+#
  
 try:
-    from distutils.core import setup
-    import py2exe, pygame
+    import pymunk, pygame
+
+    from distutils.core import setup    
+    import cx_Freeze
     from modulefinder import Module
     import glob, fnmatch
     import sys, os, shutil
-    import operator
-    import pymunk
+    import operator    
     from zipfile import ZipFile, ZIP_DEFLATED
     import zlib    
 except ImportError as message:
     raise SystemExit("Unable to load module. %s" % message)
- 
-#hack which fixes the pygame mixer and pygame font
-origIsSystemDLL = py2exe.build_exe.isSystemDLL # save the orginal before we edit it
-def isSystemDLL(pathname):
-    # checks if the freetype and ogg dll files are being included
-    if os.path.basename(pathname).lower() in ("libfreetype-6.dll", "libogg-0.dll","sdl_ttf.dll"): # "sdl_ttf.dll" added by arit.
-            return 0
-    return origIsSystemDLL(pathname) # return the orginal function
-py2exe.build_exe.isSystemDLL = isSystemDLL # override the default function with this one
- 
-class pygame2exe(py2exe.build_exe.py2exe): #This hack make sure that pygame default font is copied: no need to modify code for specifying default font
-    def copy_extensions(self, extensions):
-        #Get pygame default font
-        pygamedir = os.path.split(pygame.base.__file__)[0]
-        pygame_default_font = os.path.join(pygamedir, pygame.font.get_default_font())
- 
-        #Add font to list of extension to be copied
-        extensions.append(Module("pygame.font", pygame_default_font))
-        py2exe.build_exe.py2exe.copy_extensions(self, extensions)
 
+setup = cx_Freeze.setup
+
+def update_and_get_build_number():
+    #Update build num
+    f = open("buildnum", "r+")
+    bnum = int(f.read()) + 1
+
+    f.seek(0)
+    f.write(str(bnum))
+    f.close()
+
+    from SBA_Serv import __version__
+
+    return __version__
+ 
 class BuildExe:
     def __init__(self):
-        #Update build num
-        f = open("buildnum", "r+")
-        bnum = int(f.read()) + 1
+        self.dist_dir = 'dist'
 
-        f.seek(0)
-        f.write(str(bnum))
-        f.close()
+        #pygamedir = os.path.split(pygame.base.__file__)[0]
+        #pygame_default_font = os.path.join(pygamedir, pygame.font.get_default_font())
 
-        from SBA_Serv import __version__
-
-        #Name of starting .py
-        self.script = "SBA_Serv.py"
- 
-        #Name of program
-        self.project_name = "Space Battle Arena"
- 
-        #Project url
-        self.project_url = "http://www.mikeware.com/"
- 
-        #Version of program
-        self.project_version = __version__
- 
-        #License of the program
-        self.license = "GPLv2"
- 
-        #Auhor of program
-        self.author_name = "Michael A. Hawker"
-        self.author_email = "questions@mikeware.com"
-        self.copyright = "Copyright (c) 2012-2018 Michael A. Hawker."
- 
-        #Description
-        self.project_description = "Space Battle Arena Programming Game"
- 
-        #Icon file (None will use pygame default icon)
-        self.icon_file = None
- 
         #Extra files/dirs copied to game
         pymunk_dir = os.path.dirname(pymunk.__file__)
 
-        extra_files = self.find_data_files('GUI\\', '*.png', '*.wav', recursive=True)
-        root = [os.path.join(pymunk_dir, 'chipmunk.dll'),
-                                  "freesansbold.ttf", 
-                                  "buildnum",
-                                  "..\changelog.md", "..\README.md", "..\LICENSE", "..\COPYING"]
-        root += glob.glob("*.cfg")
-        extra_files += [('', root)]
-        print(extra_files)
-        self.extra_datas = extra_files
+        self.extra_files = [
+            ("GUI/", "GUI/"),
+            (os.path.join(pymunk_dir, 'chipmunk.dll'), 'chipmunk.dll'),
+            ("freesansbold.ttf", '.'),
+            ("buildnum", '.'),
+            ("../changelog.md", '.'),
+            ("../README.md", '.'),
+            ("../LICENSE", '.'),
+            ("../COPYING", '.'),
+        ]
+        config = glob.glob("*.cfg")
+        for f in config:
+            self.extra_files.append((f, '.'))
+        print(self.extra_files)
  
-        #Extra/excludes python modules
-        self.extra_modules = []
-        self.exclude_modules = ['ssl', '_ssl', 'tcl', 'numpy', 'distutils', 'email']
-        
-        #DLL Excludes
-        self.exclude_dll = ['CRYPT32.dll']
         #python scripts (strings) to be included, separated by a comma
         games = glob.glob('Game\\*.py')
         games.remove('Game\\__init__.py')
@@ -108,12 +72,6 @@ class BuildExe:
         games.remove('Game\\Tournaments.py')
         self.extra_scripts = ",".join(games).replace("\\", ".").replace(".py", "")
         print(self.extra_scripts)
- 
-        #Zip file name (None will bundle files in exe instead of zip file)
-        self.zipfile_name = "pylib.zip"
- 
-        #Dist directory
-        self.dist_dir ='dist'
  
     ## Code from DistUtils tutorial at http://wiki.python.org/moin/Distutils/Tutorial
     ## Originally borrowed from wxPython's setup and config files
@@ -142,7 +100,7 @@ class BuildExe:
         file_list = []
         recursive = kw.get('recursive', True)
         if recursive:
-            os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+            os.walk(srcdir, walk_helper, (file_list, wildcards))
         else:
             walk_helper((file_list, wildcards),
                         srcdir,
@@ -153,54 +111,54 @@ class BuildExe:
         if os.path.isdir(self.dist_dir): #Erase previous destination dir
             shutil.rmtree(self.dist_dir)
         
-        #Use the default pygame icon, if none given
-        if self.icon_file == None:
-            path = os.path.split(pygame.__file__)[0]
-            self.icon_file = os.path.join(path, 'pygame.ico')
- 
-        #List all data files to add
-        """
-        extra_datas = []
-        for data in self.extra_datas:
-            if os.path.isdir(data):
-                extra_datas.extend(self.find_data_files(data, '*'))
-            else:
-                extra_datas.append(('.', [data]))
-        """
-        
-        setup(
-            cmdclass = {'py2exe': pygame2exe},
-            version = self.project_version,
-            description = self.project_description,
-            name = self.project_name,
-            url = self.project_url,
-            author = self.author_name,
-            author_email = self.author_email,
-            license = self.license,
- 
-            # targets to build
-            console = [{
-                'script': self.script,
-                'icon_resources': [(0, self.icon_file)],
-                'copyright': self.copyright
-            }],
-            options = {'py2exe': {'optimize': 2, 'bundle_files': 1, 'compressed': True, \
-                                  'excludes': self.exclude_modules, 'packages': self.extra_modules, \
-                                  'dll_excludes': self.exclude_dll,
-                                  'includes': self.extra_scripts} },
-            zipfile = self.zipfile_name,
-            data_files = self.extra_datas,
-            dist_dir = self.dist_dir
-            )
+        # Dependencies are automatically detected, but it might need fine tuning.
+        build_exe_options = {
+            "build_exe": self.dist_dir,
+            "packages": ["pygame", "pymunk", "ctypes"], 
+            "includes": self.extra_scripts,
+            "excludes": ['ssl', '_ssl', 'tcl', 'tkinter', 'numpy', 'distutils', 'email'],
+            "bin_excludes": ['CRYPT32.dll'],
+            "include_files": self.extra_files
+        }
+
+        executables = [cx_Freeze.Executable(
+                script="SBA_Serv.py",
+                #icon="GUI/Graphics/icon.png",
+                copyright="Copyright (c) 2012-2020 Mikeware"
+            )]
+
+        cx_Freeze.setup(
+            name = "Space Battle Arena",
+            description = "Space Battle Arena Programming Game",
+            version = update_and_get_build_number(),
+            url = "http://battlearena.mikeware.com/",
+            author = "Michael A. Hawker",
+            author_email = "questions@mikeware.com",
+            license = "GPLv2",
+
+            requires = [
+                "pygame (==1.9.6)",
+                "pymunk (==5.6.0)"
+            ],
+
+            options = {
+                "build_exe": build_exe_options
+            },
+
+            executables = executables
+        )
         
         if os.path.isdir('build'): #Clean up build dir
             shutil.rmtree('build')
  
 if __name__ == '__main__':
     if operator.lt(len(sys.argv), 2):
-        sys.argv.append('py2exe')
+        sys.argv.append('build')
+
     BuildExe().run() #Run generation
     #raw_input("Press any key to continue") #Pause to let user see that things ends 
+
+    # package up the result and drop in final output folder
     ziparchive = ZipFile('..\\bin\\SBAserver.zip', 'w')
     path = os.getcwd()
     os.chdir('dist')
